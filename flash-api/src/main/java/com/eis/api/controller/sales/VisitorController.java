badb05ea20cf77083c46b76ee1f3bb02d8a15ae9
Neil <ycaptain@gmail.com>
2020-05-14 22:26:03 +0800

    'visitor_test'

--- flash-api/src/main/java/com/eis/api/controller/sales/VisitorController.java
+++ flash-api/src/main/java/com/eis/api/controller/sales/VisitorController.java
@@ -3,6 +3,7 @@ package com.eis.api.controller.sales;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import cn.enilu.flash.api.controller.BaseController;
@@ -18,14 +19,13 @@ public class VisitorController  extends BaseController {
 	@Autowired
     private VisitorService visitorService;
 	
-	@RequestMapping(value = "/list")
+	@RequestMapping(value = "/list",method = RequestMethod.GET)
     @RequiresPermissions(value = {Permission.VISITOR})
 	public Object list(String name) {
 		if(StringUtil.isNullOrEmpty(name)) {
 			return Rets.success(visitorService.queryAll());
 		}else {
-			return Rets.success();
+			return Rets.success(visitorService.findByVisitorName(name));
 		}
-		
 	}
 }
