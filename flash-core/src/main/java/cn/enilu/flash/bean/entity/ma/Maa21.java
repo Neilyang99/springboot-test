59a7d09ba818594a4de54b09076926bae9d3ddce
Neil <ycaptain@gmail.com>
2024-02-25 23:21:30 +0800

    Update Maa21.java

--- flash-core/src/main/java/cn/enilu/flash/bean/entity/ma/Maa21.java
+++ flash-core/src/main/java/cn/enilu/flash/bean/entity/ma/Maa21.java
@@ -23,28 +23,31 @@ public class Maa21 extends BaseEntity {
 	 */
 	private static final long serialVersionUID = 8865722491151611688L;
 	
-	private Long id; //資料表唯一識別碼
 	private String maa21002; //合約編號
 	private Long maa21003; //工程案唯一識別碼
-	private Long maa21004; //承包商唯一識別碼
-	private int maa21005; //版本
-	private String maa21006; //合約日期
-	private int maa21007; //請款%
-	private int maa21008; //稅率
-	private int maa21009; //現金付款%
-	private int maa21010; //支票付款%
-	private int maa21011; //匯款付款%
-	private int maa21012; //其他付款%
-	private String maa21013; //幣別
-	private float maa21014; //匯率
-	private double maa21015; //合約金額
-	private double maa21016; //保固金
-	private int maa21017; //訂金%
-	private int maa21018; //期款%
-	private int maa21019; //尾款%
-	private int maa21020; //資料狀態
-	private String maa21021; //發出者工號
-	private String maa21022; //發出日期時間
+	private Long maa21004; //請款公司唯一識別碼
+	private Long maa21005; //付款公司唯一識別碼
+	private int maa21006; //版本
+	private String maa21007; //合約日期
+	private int maa21008; //請款%
+	private int maa21009; //稅率
+	private int maa21010; //現金付款%
+	private int maa21011; //支票付款%
+	private int maa21012; //匯款付款%
+	private int maa21013; //其他付款%
+	private String maa21014; //幣別
+	private double maa21015; //匯率
+	private double maa21016; //合約金額
+	private double maa21017; //保固金
+	private int maa21018; //訂金%
+	private int maa21019; //期款%
+	private int maa21020; //尾款%
+	private int maa21021; //資料狀態
+	private String maa21022; //主要負責項目
+	private String maa21023; //發出者工號
+	private String maa21024; //發出日期時間
+	private String maa21025; //確認者工號
+	private String maa21026; //確認日期時間
 	
 	
 }
